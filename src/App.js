f794ee38afde5528964ceda6b7827bd6a6580e55
rikmms <ricardo.sousa@challenge.pt>
2017-09-19 17:55:51 +0100

    Organize social icons. Change profile photo.

--- src/App.js
+++ src/App.js
@@ -11,14 +11,13 @@ const userInfo = {
   name: 'Ricardo Sousa',
   description: 'Software Engineer and Full Stack Developer',
   social: [
-    { name: 'mail', link: 'mailto:sousa.ricardo10@gmail.com' },
+    { name: 'twitter', link: 'https://twitter.com/rikmms', target: '_blank', rel: 'noopener noreferrer' },
+    { name: 'linkedin', link: 'https://www.linkedin.com/in/rikmms/', target: '_blank', rel: 'noopener noreferrer' },
+    { name: 'github', link: 'https://github.com/rikmms', target: '_blank', rel: 'noopener noreferrer' },
     { name: 'medium', link: 'https://medium.com/@rikmms', target: '_blank', rel: 'noopener noreferrer' },
     { name: 'quora', link: 'https://www.quora.com/profile/Ricardo-Sousa-49', target: '_blank', rel: 'noopener noreferrer' },
     { name: 'meetup', link: 'https://www.meetup.com/members/161048452/', target: '_blank', rel: 'noopener noreferrer' },
-    { name: 'linkedin', link: 'https://www.linkedin.com/in/rikmms/', target: '_blank', rel: 'noopener noreferrer' },
-    { name: 'twitter', link: 'https://twitter.com/rikmms', target: '_blank', rel: 'noopener noreferrer' },
-    { name: 'github', link: 'https://github.com/rikmms', target: '_blank', rel: 'noopener noreferrer' },
-    { name: 'github square', link: 'https://github.com/rikmms-challenge', target: '_blank', rel: 'noopener noreferrer' }
+    { name: 'mail', link: 'mailto:sousa.ricardo10@gmail.com' }
   ]
 }
 
