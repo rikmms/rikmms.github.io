36ae4a6af3940b592f5a79b908aadb82c2ac49cb
rikmms <sousa.ricardo10@gmail.com>
2017-12-05 23:53:48 +0000

    Removed the extras section.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import 'semantic-ui-css/semantic.min.css'
 import 'animate.css'
 import './App.css'
 import React from 'react'
-import { Container, Grid } from 'semantic-ui-react'
+import { Container, Grid, Divider } from 'semantic-ui-react'
 import Bio from './components/bio'
 import Summary from './components/summary'
-// import Articles from './components/articles'
-// import Extras from './components/extras'
+import Articles from './components/articles'
 
 const userInfo = {
   photoSrc: '/assets/rikmms_photo.png', 
@@ -28,16 +27,11 @@ const summary = <div>
   <p>Also, a Trainer of Software development.</p>
   <p></p>
 </div>
-/*
+
 const articles = [
+  { date: '13/09/01', title: 'Bachelor\'s final project report (written in pt-PT)', link: 'https://drive.google.com/file/d/0BzxkMjk9Y1V8QWxEV3B0ZTY3SDA/view?usp=sharing' },
   { date: '17/10/22', title: 'How to take advantage of JavaScript’s default parameters for Dependency Injection', link: 'https://medium.freecodecamp.org/how-to-take-advantage-of-javascripts-default-parameters-for-dependency-injection-98fc423328e1' }
 ]
-
-const extras = [
-  { id: 1, content: <div>Bachelor's final project <a href='https://drive.google.com/file/d/0BzxkMjk9Y1V8QWxEV3B0ZTY3SDA/view?usp=sharing' target='_blank' rel='noopener noreferrer'>Report</a> (written in pt-PT)</div>, icon: 'university' },
-  { id: 2, content: <div>Bookshelf at the <a href='https://www.goodreads.com/review/list/23928978-ricardo-sousa?shelf=' target='_blank' rel='noopener noreferrer'>Goodreads</a></div>, icon: 'book' }
-]
-*/
 
 const App = () => {
   return (
@@ -49,6 +43,8 @@ const App = () => {
           </Grid.Column>
           <Grid.Column width={10} className="animated slideInUp">
             <Summary summary={ summary } />
+            <Divider />
+            <Articles articles={ articles } />
           </Grid.Column>
         </Grid>
       </Container>
